316890a69b8db17d2c086fd09829ed9183a3e33d
smil3cz <jan.melicherik@email.cz>
2021-03-03 15:08:20 +0100

    Set up of OAuth proces

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import App from "./components/App/App";
+import reducers from "./reducers";
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 const rootElement = document.querySelector("#root");
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
