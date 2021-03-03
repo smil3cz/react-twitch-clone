48eeff8d9893817ced226252c8806787d372925a
smil3cz <jan.melicherik@email.cz>
2021-03-03 09:40:09 +0100

    simple example router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo<button>Click Me!</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
