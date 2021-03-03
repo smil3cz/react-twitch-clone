dee64cbd0bfd9b195fbb164da3a06d197197710c
smil3cz <jan.melicherik@email.cz>
2021-03-03 11:07:59 +0100

    Create boiler plates for each individual pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,20 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne<Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo<Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
+import StreamCreate from "../streams/StreamCreate";
+import StreamEdit from "../streams/StreamEdit";
+import StreamDelete from "../streams/StreamDelete";
+import StreamList from "../streams/StreamList";
+import StreamShow from "../streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
       </Router>
     </div>
   );
