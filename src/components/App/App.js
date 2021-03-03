2167e9253839492fa4f3ab589ebdd77a2d070cb3
smil3cz <jan.melicherik@email.cz>
2021-03-03 11:26:58 +0100

    Router setup finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+
 import StreamCreate from "../streams/StreamCreate";
 import StreamEdit from "../streams/StreamEdit";
 import StreamDelete from "../streams/StreamDelete";
 import StreamList from "../streams/StreamList";
 import StreamShow from "../streams/StreamShow";
+import Header from "../Header/Header";
 
 const App = () => {
   return (
-    <div>
+    <div classname="ui container">
       <Router>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
-        <Route path="/streams/delete" component={StreamDelete} />
-        <Route path="/streams/show" component={StreamShow} />
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
       </Router>
     </div>
   );
